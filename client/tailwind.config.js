dce6880210a6f9e7a54cc58594bf0ea137677b44
Vitaly Kuprin <vitas_s@inbox.lv>
2025-02-15 09:10:30 +0100

    chore: add prettier and deps skeleton

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
       colors: {
@@ -13,5 +11,4 @@ export default {
     },
   },
   plugins: [],
-}
-
+};
